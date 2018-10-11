3de82962cb002e2feeceddb18f2605342225e60b
koniormariusz90@o2.pl <koniormariusz90@o2.pl>
2018-10-11 22:54:26 +0200

    FIX: Bugs

--- src/app.js
+++ src/app.js
@@ -1,13 +1,8 @@
 import './assets/scss/app.scss';
 import $ from 'cash-dom';
 import 'es6-promise';
-// "whatwg-fetch" library support browser
-// Chrome
-// Firefox
-// Safari 6.1+
-// Internet Explorer 10+
 import {fetch as fetchPolyfill} from 'whatwg-fetch'
-
+import validator from 'validator';
 
 export class App {
 
@@ -17,47 +12,67 @@ export class App {
 
         $('.load-username').on('click', function (e) {
             let userName = $('.username.input').val();
-            // an asynchronous query requires a promise
-            self.getUser(userName)
-                .then(function (body) {
-                    if (body) {
-                        self.update_profile(body);
-                        return self.getUserRepo(userName)
-                            .then(function (repository) {
-                                if (repository) {
-                                    console.log("repository", repository);
-                                    self.update_repository(repository);
-                                }
-                                else {
-                                    // handle the case when we receive information about the lack of a user in the database
-                                    alert("The user you are looking for doesn't have repository!");
-                                }
-                            })
-                    }
-                    else {
-                        // handle the case when we receive information about the lack of a user in the database
-                        alert("The user you are looking for does not exist!");
-                    }
-                });
+            if (self.checkValueIsValid(userName)) {
+                self.setBorderForInutSearch();
+                self.getUser(userName)
+                    .then(function (body) {
+                        if (body) {
+                            self.update_profile(body);
+                            self.getUserRepo(userName)
+                                .then(function (repository) {
+                                    if (repository) {
+                                        console.log("repository", repository);
+                                        self.update_repository(repository);
+                                    }
+                                    else {
+                                        alert("The user you are looking for doesn't have repository!");
+                                    }
+                                });
+                            self.getUserPublic(userName)
+                                .then(function (publicion) {
+                                    if (publicion) {
+                                        console.log("Public", publicion);
+                                        self.update_history(publicion);
+                                    }
+                                    else {
+                                        alert("The user you are looking for doesn't have repository!");
+                                    }
+                                })
+                        }
+                        else {
+                            alert("The user you are looking for does not exist!");
+                        }
+                    });
+            }
+            else {
+                self.setBorderForInutSearch(true);
+                $('.username.input').attr('style', "border: 1px solid red");
+            }
         })
     }
 
+    setBorderForInutSearch(setBorder) {
+        $('.username.input').attr('style', "border: " + setBorder ? "1px solid red" : "none");
+    }
+
+    checkValueIsValid(value) {
+        return validator.isAlphanumeric(value.replace(/-/g, '').replace(/_/g, ''))
+    }
+
     getUser(userName) {
-        return fetchPolyfill(`https://api.github.com/users/${userName}`)
-            .then(function (response) {
-                if (response.status >= 400) {
-                    return null;
-                }
-                return response.json();
-            })
-            .then(response => {
-                return response;
-            })// ensure basic handling of the exception
-            .catch(error => console.error('Error:', error));
+        return this._getResource(`https://api.github.com/users/${userName}`)
+    }
+
+    getUserPublic(userName) {
+        return this._getResource(`https://api.github.com/users/${userName}/events/public`)
     }
 
     getUserRepo(userName) {
-        return fetch(`https://api.github.com/users/${userName}/repos`)
+        return this._getResource(`https://api.github.com/users/${userName}/repos`)
+    }
+
+    _getResource(resource) {
+        return fetchPolyfill(resource)
             .then(function (response) {
                 if (response.status >= 400) {
                     return null;
@@ -69,8 +84,30 @@ export class App {
             })
     }
 
+    update_history(history) {
+        $('#user-timeline').html("");
+        for (let i = 0; i < history.length; i++) {
+            $('#user-timeline').append(' <div class="timeline-item">\n' +
+                '          <div class="timeline-marker"></div>\n' +
+                '          <div class="timeline-content">\n' +
+                '            <p class="heading">' + history[i].created_at + '</p>\n' +
+                '            <div class="content">\n' +
+                '                  <span class="gh-username">\n' +
+                '                    <img src="' + history[i].actor.avatar_url + '"/>\n' +
+                '                    <a href="https://github.com/' + history[i].actor.display_login + '">' + history[i].actor.display_login + '</a>\n' +
+                '                  </span>\n' +
+                '              closed\n' +
+                '              <p class="repo-name">\n' +
+                '                <a href="https://github.com/' + history[i].repo.name + '">' + history[i].repo.name + '</a>\n' +
+                '              </p>\n' +
+                '            </div>\n' +
+                '          </div>\n' +
+                '        </div>');
+        }
+    }
 
     update_repository(repository) {
+        $('#repository').html("");
         for (let i = 0; i < repository.length; i++) {
             $('#repository').append("<li>" + repository[i].name + "</li>");
         }
